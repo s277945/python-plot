4f2ac6da62ace623a57498d7eef4bb5216eff837
s277945 <s277945@studenti.polito.it>
2023-12-07 12:16:22 +0100

    Plot script wip

--- filePlotSingleUnordered.py
+++ filePlotSingleUnordered.py
@@ -1,30 +1,51 @@
 
 import matplotlib.pyplot as plt 
+import numpy as np
 
-# class rowData :
-#     def __init__(self, l, hl, ll, op, sp):
-#         self.latency = l
-#         self.highLatency = hl
-#         self.lowLatency = ll
-#         self.oldPacket = op
-#         self.slowPacket = sp
+class rowData :
+    def __init__(self, name, latency):
+        self.name = name
+        self.latency = latency
 
-tracks = [] 
-objects = [] 
-groups = [] 
-status = [] 
-latency = [] 
-highLatency = [] 
-lowLatency = [] 
-oldPacket = [] 
-slowPacket = [] 
-  
+data = []
+# tracks = [] 
+# objects = [] 
+# groups = [] 
+# status = [] 
+# latency = [] 
+# highLatency = [] 
+# lowLatency = [] 
+# oldPacket = [] 
+# slowPacket = [] 
+names = []
+track_ids = []
+tracks = []
+names_tracks = []
+color = []
+color_tracks = []
 f = open('test.txt','r') 
 for row in f: 
     row = row.split(';') 
-    tracks.append(row[0]) 
+    if(row[0].isnumeric()) :        
+        # if row has track id and latency value
+        if(row[4].isnumeric()) : 
+            # add row data (received packet data) to general data array
+            data.append(rowData(row[0] + "-" + int(row[2]), int(row[4])))        
+            # add track to track array if not present and the received packet data to the individual track data array
+            if row[0] not in tracks :
+                track_ids.append(row[0])
+                tracks.append([])
+            tracks[track_ids.index(row[0])].append(rowData(int(row[2]), int(row[4])))
+        else if(row[3] == 'too slow') :
+            # add row data (received packet data) to general data array
+            data.append(rowData(row[0] + "-" + int(row[2]), int(row[4])))        
+            # add track to track array if not present and the received packet data to the individual track data array
+            if row[0] not in tracks :
+                track_ids.append(row[0])
+                tracks.append([])
+            tracks[track_ids.index(row[0])].append(rowData(int(row[2]), int(row[4])))
+        
     objects.append(int(row[1])) 
-    groups.append(int(row[2])) 
     status.append(row[3]) 
     latency.append(int(row[4])) 
     highLatency.append(int(row[4]))
@@ -32,6 +53,10 @@ for row in f:
     oldPacket.append(int(row[4]))
     slowPacket.append(int(row[4]))
   
+mask1 = y < 0.5
+mask2 = y >= 0.5
+
+fig, axs = plt.subplots(2)
 plt.bar(names, marks, color = 'g', label = 'File Data') 
   
 plt.xlabel('Student Names', fontsize = 12) 

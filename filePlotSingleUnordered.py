90c7fe2519f25968377292862a4f057985cf77ab
s277945 <s277945@studenti.polito.it>
2023-12-08 00:07:33 +0100

    Plotting wip

--- filePlotSingleUnordered.py
+++ filePlotSingleUnordered.py
@@ -28,49 +28,66 @@ tracks = []
 f = open('test.txt','r') 
 for row in f: 
     row = row.strip('\n').split(';') 
-    print(row)
+    # print(row)
     if(row[0].isnumeric()) :        
         # if row has track id and latency value
-        if(4 < len(row) and row[4].isnumeric()) : 
-            # add row data (received packet data) to general data array
-            data.append(rowData(row[0] + "-" + row[2], int(row[4]), (0.1, 0.1, 0.8, 1)))        
+        if(4 < len(row) and row[4].isnumeric() and int(row[2]) > 0) : 
             # add track to track array if not present and the received packet data to the individual track data array
+            # add row data (received packet data) to general data array
             if row[0] not in track_ids :
                 track_ids.append(row[0])
                 tracks.append([])
-            tracks[track_ids.index(row[0])].append(rowData(int(row[2]), int(row[4]), (0.1, 0.1, 0.8, 1)))
-        elif(row[3] == 'too slow\n') :
+            if(int(row[4]) > 35 ) :
+                data.append(rowData(row[0] + "-" + row[2], int(row[4]), (0.8, 0.1, 0.1, 1)))   
+                tracks[track_ids.index(row[0])].append(rowData(row[2], int(row[4]), (0.8, 0.1, 0.1, 1)))
+            else :                 
+                data.append(rowData(row[0] + "-" + row[2], int(row[4]), (0.1, 0.1, 0.8, 1))) 
+                tracks[track_ids.index(row[0])].append(rowData(row[2], int(row[4]), (0.1, 0.1, 0.8, 1)))
+        elif(row[3] == 'too slow') :
             # change item color if too slow packet
             row_name = row[0] + "-" + row[2]
             index = getIndex(data, row_name)
-            if( index>=0 ) : 
-                data[index].setColor((0.6, 0.1, 0.3, 1)) 
+            if( index >= 0 ) : 
+                data[index].setColor((0.6, 0.0, 0.4, 1)) 
                 index2 = getIndex(tracks[track_ids.index(row[0])], row_name)
+                print(index2)
                 if( index2>=0 ) : 
-                    tracks[track_ids.index(row[0])][index2].setColor((0.6, 0.1, 0.3, 0.7))
-        elif(row[3] == 'too old\n') :
+                    tracks[track_ids.index(row[0])][index2].setColor((0.6, 0.0, 0.4, 1))
+        elif(row[3] == 'too old') :
             # change item color if too old packet
             row_name = row[0] + "-" + row[2]
             index = getIndex(data, row_name)
             if( index>=0 ) : 
-                data[index].setColor((0.6, 0.1, 0.3, 1)) 
+                data[index].setColor((0.6, 0.2, 0.2, 1)) 
                 index2 = getIndex(tracks[track_ids.index(row[0])], row_name)
                 if( index2>=0 ) : 
-                    tracks[track_ids.index(row[0])][index2].setColor((0.6, 0.3, 0.1, 0.7))
+                    tracks[track_ids.index(row[0])][index2].setColor((0.6, 0.2, 0.2, 1))
   
 print(len(track_ids) + 1)
 print("Tracks found: " + str(track_ids))
 fig, axs = plt.subplots(len(tracks) + 1)
 fig.suptitle('moq-js latency test', fontsize = 20)
-axs[0].bar(list(map(lambda node: node.name, data)), list(map(lambda node: node.latency, data)), color = list(map(lambda node: node.color, data)))
+ticks0 = []
+for i, elem in enumerate(data) : 
+    axs[0].bar(elem.name, elem.latency, color = elem.color)
+    # if(i % 50 == 0) :
+    #     ticks0.append(elem)
 axs[0].set_title("All packets")
-axs[0].set_xlabel('Object sequence number', fontsize = 12)
 axs[0].set_ylabel('Latency(ms)', fontsize = 12)
-for pos, id in enumerate(track_ids) :
-    axs[pos + 1].bar(list(map(lambda node: node.name, data)), list(map(lambda node: node.latency, data)), color = list(map(lambda node: node.color, data)))    
-    axs[pos + 1].set_title('Track' + id)    
-    axs[pos + 1].set_xlabel('Object sequence number', fontsize = 12)
-    axs[pos + 1].set_ylabel('Latency(ms)', fontsize = 12)
+axs[0].set_xticks(axs[0].get_xticks()[::50])
+
+for pos, id in enumerate(track_ids) :    
+    for elem in tracks[pos] : 
+        axs[pos + 1].bar(elem.name, elem.latency, color = elem.color)
+    if(pos + 1 == len(track_ids))  :
+        axs[pos + 1].set_xlabel('Object sequence number', fontsize = 12)
+        axs[pos + 1].set_ylabel('Video', fontsize = 12)
+    else :
+        axs[pos + 1].set_ylabel('Audio', fontsize = 12)
+        axs[pos + 1].set_xticks(axs[pos + 1].get_xticks()[::50])
  
+props = {"rotation" : 45}
+for ax in axs : 
+    plt.setp(ax.get_xticklabels(), **props)
 plt.legend() 
 plt.show() 

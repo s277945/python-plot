72bf41bd3847bb077a1b7f2af518b6593557eabb
s277945 <s277945@studenti.polito.it>
2024-05-03 17:36:32 +0200

    wireshark json converter added new classes

--- wiresharkMoqjsConversion.py
+++ wiresharkMoqjsConversion.py
@@ -2,6 +2,20 @@ import argparse
 from functools import reduce
 import json
 
+class Stream :
+    def __init__(self, id, packets = [], moqHeader = "", occurrences = 1) -> None:
+        self.id = id
+        self.packets = packets
+        self.moqHeader = moqHeader
+        self.occurrences = occurrences
+    
+class Packet :
+    def __init__(self, id, packets = [], moqHeader = "", occurrences = 1) -> None:
+        self.id = id
+        self.packets = packets
+        self.moqHeader = moqHeader
+        self.occurrences = occurrences
+
 dictfilt = lambda x, y: dict([ (i,x[i]) for i in x if i in set(y) ])
 wanted_keys = ('quic.stream_data_raw')
 # Opening JSON file
@@ -11,24 +25,33 @@ f = open('test2.json')
 # a dictionary
 data = json.load(f)
  
+streams = {}
+
 # Iterating through the json
 # list
 for i in data:
-    filteredData = i['_source']['layers']['quic']['quic.frame']
-    length = 0
-    offset = 0
-    streamId = None
-    data = None
+    filteredData = i['_source']['layers']['quic']
     for key, entry in filteredData.items() :
-        if key == 'quic.stream.stream_id_raw' :
-            streamId = entry[0]
-        if key == 'quic.stream.offset_raw' :
-            offset = int(entry[0], 16)
-        if key == 'quic.stream_data_raw' :
-            data = entry[0]
-            length = entry[2]
-    if offset < 16 and streamId is not None :
-        print("Stream id:", streamId, "\nOffset:", offset, "\nLength:", length, "\nData:", data)
-    
+        if key == 'quic.frame' :
+            filteredData = entry
+            length = 0
+            offset = 0
+            streamId = None
+            data = None
+            for key, entry in filteredData.items() :
+                if key == 'quic.stream.stream_id_raw' :
+                    streamId = entry[0]
+                if key == 'quic.stream.offset_raw' :
+                    offset = int(entry[0], 16)
+                if key == 'quic.stream_data_raw' :
+                    data = entry[0]
+                    length = entry[2]
+            if offset < 16 and streamId is not None :
+                print("Stream id:", streamId, "\nOffset:", offset, "\nLength:", length, "\nData:", data)
+                if streamId not in streams : streams[streamId] = Stream(streamId)
+                else :
+                    stream = streams[streamId]
+                    # continue
+            
 # Closing file
 f.close()

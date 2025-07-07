3318de945cabce715105bca61c7e34fabaf5e258
s277945 <s277945@studenti.polito.it>
2025-07-07 18:02:52 +0200

    parallel plotting utility

---
+++ batch_plot_loss_parallel.py
@@ -0,0 +1,33 @@
+import os
+import glob
+import subprocess
+from concurrent.futures import ThreadPoolExecutor
+
+LOGDIR = "logs_test"
+OUTDIR = "output_plots"
+SCRIPT = "plotMoqjsTimestamp.py"
+THREADS = 16  # Modifica qui il grado di parallelismo
+
+os.makedirs(OUTDIR, exist_ok=True)
+logfiles = glob.glob(os.path.join(LOGDIR, "*.txt"))
+
+def worker(logfile):
+    basename = os.path.basename(logfile)
+    outname = os.path.splitext(basename)[0] + "_cumuloss.png"
+    print(f"Plotting {basename} ...")
+    subprocess.run([
+        "h:/Tesi/python-plot/Scripts/python.exe", SCRIPT,
+        "-sks", "150",
+        "-mh", "auto",
+        "-shl", "true",
+        "-f", logfile,
+        "-sf", "true"
+    ])
+    # Sposta il file se prodotto in cwd
+    if os.path.exists(outname):
+        os.replace(outname, os.path.join(OUTDIR, outname))
+
+with ThreadPoolExecutor(max_workers=THREADS) as pool:
+    pool.map(worker, logfiles)
+
+print(f"\nFatto! Trovi tutti i plot in {OUTDIR}")
